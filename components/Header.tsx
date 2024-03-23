17251fbc9f185e83d7239be31137f293ad637ca1
Wisdom Ochei <wisdomofgod2003@gmail.com>
2024-03-23 11:28:39 +0100

    Done with Sidebar and Header Basic UI min 43:25

---
+++ components/Header.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { twMerge } from "tailwind-merge";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+  const handleLogout = () => {};
+
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        {/* Desktop Header */}
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.back()}
+          >
+            <RxCaretLeft size={35} />
+          </button>
+          <button
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            <RxCaretRight size={35} />
+          </button>
+        </div>
+
+        {/* Mobile Header */}
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+
+        {/* Login status */}
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button>Sign Up</Button>
+            </div>
+          </>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
