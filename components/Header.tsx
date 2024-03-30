3e3e9591491384cc37310a47ae87506f905555ca
Wisdom Ochei <wisdomofgod2003@gmail.com>
2024-03-30 11:53:49 +0100

    Added Supabase client to the project 1:28:48

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,11 +53,19 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button>Sign Up</Button>
+              <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Log In
+              </Button>
             </div>
           </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
